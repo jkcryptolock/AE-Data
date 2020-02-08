9bf65ec0a9c5d813d0ca000364d9b5e36f8d7624
jkcryptolock <jkconno@gmail.com>
2020-02-08 13:37:51 -0600

    added axios route to pull AE data on component mount

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import Select from './Components/Select';
 import Table from './Components/Table';
 import './App.css';
+import Axios from 'axios';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,6 +13,20 @@ export default class App extends React.Component {
       accountExecs: [],
       companyData: []
     };
+  }
+
+  componentDidMount() {
+    this.fetchAccountExecs();
+  }
+
+  fetchAccountExecs() {
+    Axios.get('https://codechallenges-accountexecutiveapi.azurewebsites.net/api/users')
+    .then(result => {
+      this.setState( { accountExecs: result });
+    })
+    .catch(error => {
+      console.log('Error with fetching AEs: ', error);
+    });
   }
   
   render() {
--- src/Components/Select.js
+++ src/Components/Select.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Select = (props) => {
 
     return (
-        <div class="select"></div>
+        <div className="select"></div>
     );
 }
 
